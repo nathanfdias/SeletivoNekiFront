acf37359ac12463b7281a4dab7c626a462d04034
Nathan Dias <nathanfaria9@gmail.com>
2023-01-06 17:41:03 -0300

    Perfil Home estilizado: Pronto para consumo

--- projeto/src/Pages/Home/index.js
+++ projeto/src/Pages/Home/index.js
@@ -1,14 +1,127 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
+import React from "react";
+import Navbar from "../../components/Navbar";
 import "../Home/index.css";
 
 export function Home() {
- return (
-   <>
-        <div className='home-container'>
-          <Navbar />
-
+  return (
+    <>
+      <div className="home-container">
+        <Navbar />
+        <div className="home-boxes-container">
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
+          <div className="home-itens-map">
+            <div className="home-skill-box-title">
+              <p>Skill: HTML</p>
+              <p>Level: 5</p>
+            </div>
+            <img
+              className="skill-image"
+              src={
+                "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw"
+              }
+            ></img>
+            <button className="skill-box-button">Level Edit</button>
+            <button className="skill-box-button">Delete Skill</button>
+          </div>
         </div>
-   </>
+      </div>
+    </>
   );
 }
